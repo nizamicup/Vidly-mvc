3ebb9d7ad1d7e7a5652225b5e2e40e30cd4b1b9e
Nizam <Nizam>
2017-04-14 22:33:28 +0200

    AddBirthDateToCustomer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
