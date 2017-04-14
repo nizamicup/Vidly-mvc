a3cd36bf0c2defa8934f6464ddab118b62bf211c
Nizam <Nizam>
2017-04-14 20:28:02 +0200

    AddDiscountRateColumnToCustomer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,10 +24,10 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
-
+        
         public ActionResult Detail(int? id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
