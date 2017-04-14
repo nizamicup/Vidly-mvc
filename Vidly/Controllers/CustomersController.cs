1a8c3887d59b6b906e27b6522df15b4aadfd597f
Nizam <Nizam>
2017-04-14 15:05:56 +0200

    initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer>
+            {
+                new Customer{Name= "John Smith"},
+                new Customer{Name= "Mary Williams"}
+            };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            
+            return View(customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = customers[id];
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Randome()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer1"},
+                new Customer{Name="Customer2"}
+            };
+
+            var viewModel = new RandomeMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie{Name="Shrek!"},
+                new Movie{Name="King Arthur"}
+            };
+            return View(movies);
+        }
+    }
+}
